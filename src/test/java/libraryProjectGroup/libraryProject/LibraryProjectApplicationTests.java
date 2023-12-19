70f4633dafe3cbb673bdf535f5de74a56388d64b
ZeynepDerin <zeynep.derin@brightstraining.com>
2023-12-19 17:26:59 +0100

    Initial commit

---
+++ src/test/java/libraryProjectGroup/libraryProject/LibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package libraryProjectGroup.libraryProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
