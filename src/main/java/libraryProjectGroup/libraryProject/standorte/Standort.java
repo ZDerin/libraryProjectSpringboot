38b43e72973bae263472b6cb2c727b233424fd42
VivianeNeubert <viviane.neubert@web.de>
2024-01-05 11:23:51 +0100

    Standort Klasse erstellt

--- src/main/java/libraryProjectGroup/libraryProject/standorte/Standort.java
+++ src/main/java/libraryProjectGroup/libraryProject/standorte/Standort.java
@@ -1,6 +1,7 @@
 package libraryProjectGroup.libraryProject.standorte;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -8,5 +9,21 @@ import jakarta.persistence.Table;
 public class Standort {
     private String name;
 
+    @Id
+    private Long id;
 
+    public Standort() {
+    }
+
+    public Standort(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
